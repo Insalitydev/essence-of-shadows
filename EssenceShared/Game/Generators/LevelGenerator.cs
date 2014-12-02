09974bedde561bc069130147f1faab7d5319cdda
alex1313 <sav.1209@gmail.com>
2014-12-03 00:55:31 +0500

    Changed generator logic

--- EssenceShared/Game/Generators/LevelGenerator.cs
+++ EssenceShared/Game/Generators/LevelGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LevelGenerator {
     public static class Generator {
@@ -7,7 +8,7 @@ namespace LevelGenerator {
 
         public enum LevelType {
             City,
-            Forest,
+            Cave,
             Desert
         }
 
@@ -28,66 +29,85 @@ namespace LevelGenerator {
             {"dirt", '.'},
         };
 
-        private static double _maxNoise;
-        private static double _minNoise = 2.0;
-        private static int[,] _perlinArray;
+        private static double _maxNoise = 1.2456439435482;
+        private static double _minNoise = 0.209547579288483;
+        private static double[,] _perlinArray;
+        private static int[,] _intPerlinArray;
 
-        public static List<string> GenerateLevel(int width, int height, LevelType type)
-        {
+        public static List<string> GenerateLevel(int width, int height, LevelType type) {
             Width = width;
             Height = height;
-            Type = type; 
+            Type = type;
             MapTile = new char[Height, Width];
-            //switch (Type)
-            //{
-            //    case LevelType.Desert:
-            //        for (int i = 0; i < Height; i++)
-            //            for (int j = 0; j < Width; j++)
-            //                MapTile[i, j] = Tile["sand"];
-            //        break;
-            //    case LevelType.Forest:
-            //        for (int i = 0; i < Height; i++)
-            //            for (int j = 0; j < Width; j++)
-            //                MapTile[i, j] = Tile["tree"];
-            //        break;
-            //    case LevelType.City:
-            //        for (int i = 0; i < Height; i++)
-            //            for (int j = 0; j < Width; j++)
-            //                MapTile[i, j] = Tile["building"];
-            //        break;
-            //}
+            _perlinArray = new double[Height, Width];
+            _intPerlinArray = new int[Height, Width];
 
-            _perlinArray = new int[Width, Height];
             var noise = new PerlinNoise2D();
             for (int i = 0; i < Height; i++) {
                 for (int j = 0; j < Width; j++) {
-                    switch (Type) {
-                        case LevelType.Desert:
-                            MapTile[i, j] = Tile["sand"];
-                            break;
-                        case LevelType.Forest:
-                            MapTile[i, j] = Tile["tree"];
-                            break;
-                        case LevelType.City:
-                            MapTile[i, j] = Tile["building"];
-                            break;
-                    }
-
-                    double tile = noise.PerlinNoise(i, j);
-                    if (noise.PerlinNoise(i, j) > _maxNoise)
-                        _maxNoise = tile;
-                    if (noise.PerlinNoise(i, j) < _minNoise)
-                        _minNoise = tile;
-
-                    //perlinArray[i, j] = intTile;
-                    Console.Write(noise.PerlinNoise(i, j) + " ");
+                    _perlinArray[i, j] = noise.PerlinNoise(i, j);
+                    if (_perlinArray[i, j] > _maxNoise)
+                        _maxNoise = _perlinArray[i, j];
+                    else if (_perlinArray[i, j] < _minNoise)
+                        _minNoise = _perlinArray[i, j];
                 }
-
-                Console.WriteLine();
             }
 
-            Console.WriteLine("{0}\n{1}", _maxNoise, _minNoise);
-            return null;
+            var valueOfWater = 0;
+            int srcX = 0, srcY = 0, dstX = 0, dstY = 0;
+            var maxToMin = _maxNoise/_minNoise;
+            for (int i = 0; i < Height; i++) {
+                for (int j = 0; j < Width; j++) {
+                    _intPerlinArray[i, j] = (int) ((_perlinArray[i, j] - _minNoise)*maxToMin);
+                    //if (_intPerlinArray[i, j] == valueOfWater) {
+                    //    dstX = srcX;
+                    //    dstY = srcY;
+                    //    srcX = i;
+                    //    srcY = j;
+                    //}
+                }
+            }
+
+            //for (int i = srcX; i > dstX; i--) {
+            //    dstY--;
+            //    _intPerlinArray[i, dstY] = valueOfWater;
+            //        if (_intPerlinArray[i - 1, dstY] == valueOfWater) {
+            //            i--;
+            //            _intPerlinArray[i, dstY] = valueOfWater;
+            //        }
+            //    if (dstY - 1 >= 0) {
+            //        if (_intPerlinArray[i, dstY - 1] == valueOfWater) {
+            //            dstY--;
+            //            _intPerlinArray[i, dstY] = valueOfWater;
+            //        }
+            //        if (_intPerlinArray[i - 1, dstY - 1] != valueOfWater) continue;
+            //        i--;
+            //        dstY--;
+            //        _intPerlinArray[i, dstY] = valueOfWater;
+            //    }
+            //}
+
+            //for (int i = 0; i < Height; i++)
+            //{
+            //    for (int j = 0; j < Width; j++)
+            //    {
+            //        Console.Write(_intPerlinArray[i, j].ToString());
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            var tileList = new List<string>();
+            for (int i = 0; i < Height; i++) {
+                var line = "";
+                for (int j = 0; j < Width; j++) {
+                    if (_intPerlinArray[i, j] == 0)
+                        line += '~';
+                    else
+                        line += '#';
+                }
+                tileList.Add(line);
+            }
+            return tileList;
         }
 
         private static void Output() {
